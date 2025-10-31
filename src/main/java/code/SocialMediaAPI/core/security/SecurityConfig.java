72a1c68ccf41a2ce876988c0c82783b79ec87216
r9988.2588 <r9988.2588@gmail.com>
2025-10-31 08:46:00 +0000

    add: SecurityFilterChain method

--- src/main/java/code/SocialMediaAPI/core/security/SecurityConfig.java
+++ src/main/java/code/SocialMediaAPI/core/security/SecurityConfig.java
@@ -2,13 +2,30 @@ package code.SocialMediaAPI.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    // SecurityFilterChain is used to configure the security of the application
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/admin/auth/login").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
+                .build();
+    }
 
     // BCryptPasswordEncoder is used to hash the password in the db
     @Bean
