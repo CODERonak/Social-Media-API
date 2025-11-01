dca584a8ad0eed6d9cc65d2c1489aca9d5d45cbc
r9988.2588 <r9988.2588@gmail.com>
2025-11-01 09:36:17 +0000

    create: user details and details service

---
+++ src/main/java/code/SocialMediaAPI/core/security/MyUserDetails.java
@@ -0,0 +1,37 @@
+package code.SocialMediaAPI.core.security;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import code.SocialMediaAPI.usermodule.model.User;
+import lombok.AllArgsConstructor;
+
+// MyUserDetails class implements UserDetails interface from Spring Security
+// It is used to store user details for authentication and authorization
+@AllArgsConstructor
+public class MyUserDetails implements UserDetails {
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRole().toString()));
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+}
---
+++ src/main/java/code/SocialMediaAPI/core/security/MyUserDetailsService.java
@@ -0,0 +1,22 @@
+package code.SocialMediaAPI.core.security;
+
+import code.SocialMediaAPI.usermodule.model.User;
+import code.SocialMediaAPI.usermodule.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class MyUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Not found"));
+
+        return new MyUserDetails(user);
+    }
+}
